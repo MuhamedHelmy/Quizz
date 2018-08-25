0af9b62352458ed33c73334cfc712e2b0d4b1afb
mohamedabdohelmy <mohamedhelmy23796@gmail.com>
2018-08-25 23:20:49 +0200

    New

--- app/src/main/java/com/example/mohamed_abdelsamad/quizz/MainActivity.java
+++ app/src/main/java/com/example/mohamed_abdelsamad/quizz/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.mohamed_abdelsamad.quizz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.RadioButton;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -15,27 +15,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-  // button  click method
+
+    // button  click method
     public void checkAnswer(View v) {
         int score = getScore();
-        Toast.makeText(MainActivity.this, "Your score is : " + score
+        Toast.makeText(MainActivity.this, getString(R.string.message2) + score
                 , Toast.LENGTH_SHORT).show();
     }
+
     //getscore method
     private int getScore() {
         int score = 0;
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroub1);
-        RadioGroup radioGroup1 = (RadioGroup) findViewById(R.id.radiogrobe2);
-        RadioGroup radioGroup2 = (RadioGroup) findViewById(R.id.radiogroub3);
-        CheckBox checkBox = (CheckBox) findViewById(R.id.q4a1);
-
-
-        CheckBox checkBox2 = (CheckBox) findViewById(R.id.q4a3);
-        CheckBox checkBox3 = (CheckBox) findViewById(R.id.q5a1);
-
-        CheckBox checkBox4 = (CheckBox) findViewById(R.id.q5a2);
-        CheckBox checkBox5 = (CheckBox) findViewById(R.id.q5a3);
-
+        RadioGroup radioGroup = findViewById(R.id.radiogroub1);
+        RadioGroup radioGroup1 = findViewById(R.id.radiogrobe2);
+        RadioGroup radioGroup2 = findViewById(R.id.radiogroub3);
+        CheckBox checkBox = findViewById(R.id.q4a1);
+        CheckBox checkBox2 = findViewById(R.id.q4a3);
+        CheckBox checkBox3 = findViewById(R.id.q5a1);
+        CheckBox checkBox4 = findViewById(R.id.q5a2);
+        CheckBox checkBox5 = findViewById(R.id.q5a3);
+        CheckBox checkBox6 = findViewById(R.id.q5a4);
+        CheckBox checkBox7 = findViewById(R.id.q4a2);
+        EditText answer = findViewById(R.id.edittext);
 
         if (radioGroup.getCheckedRadioButtonId() == R.id.q1a2) {
             score += 1;
@@ -46,12 +47,18 @@ public class MainActivity extends AppCompatActivity {
         if (radioGroup2.getCheckedRadioButtonId() == R.id.q3a1) {
             score += 1;
         }
-        if (checkBox.isChecked() && checkBox2.isChecked()) {
+        if (checkBox.isChecked() && checkBox2.isChecked() && !checkBox7.isChecked()) {
             score += 1;
         }
-        if (checkBox3.isChecked() && checkBox4.isChecked() && checkBox5.isChecked()) {
+        if (checkBox3.isChecked() && checkBox4.isChecked() && checkBox5.isChecked() && !checkBox6.isChecked()) {
             score += 1;
         }
+        if (answer.getText().toString().equalsIgnoreCase("Relative") || answer.getText().toString().equalsIgnoreCase("Relative layout") ||
+                answer.getText().toString().equalsIgnoreCase(" Relative layout") || answer.getText().toString().equalsIgnoreCase(" Relative")
+                || answer.getText().toString().equalsIgnoreCase("Relative ")) {
+            score += 1;
+        } else
+            Toast.makeText(MainActivity.this, getString(R.string.toastmessage1), Toast.LENGTH_SHORT).show();
 
         return score;
     }
